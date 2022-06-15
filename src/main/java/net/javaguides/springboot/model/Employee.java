1a415a6eecd84faa647528dec038fa9983e88ee8
ankitkushwah <ankitkushwahji159@gmail.com>
2022-06-15 12:03:48 +0530

    my first commit

---
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -0,0 +1,65 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+@EnableAutoConfiguration
+@Entity
+@Table(name="employee")
+
+
+public class Employee {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	@Column(name="first_name")
+	private String fristName;
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email_id")
+	private String emailId;
+	
+	public Employee() {
+		
+	}
+	public Employee(String fristName, String lastName, String emailId) {
+		super();
+		this.fristName = fristName;
+		this.lastName = lastName;
+		this.emailId = emailId;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFristName() {
+		return fristName;
+	}
+	public void setFristName(String fristName) {
+		this.fristName = fristName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	
+
+}
